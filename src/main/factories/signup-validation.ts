8df73fedcdd90c8a9a699d769d2ae044c4e615f3
TonySandro <tonysduarte101@gmail.com>
2022-02-14 19:11:33 -0300

    refactor: move required field validation to ge ValidationComposite

---
+++ src/main/factories/signup-validation.ts
@@ -0,0 +1,12 @@
+import { Validation } from "../../presentation/helpers/validators/validation";
+import { RequiredFieldValidation } from "../../presentation/helpers/validators/required-field-validation";
+import { ValidationComposite } from "../../presentation/helpers/validators/validation-compisite";
+
+
+export const makeSignUpValidation = (): ValidationComposite => {
+    const validations: Validation[] = []
+    for (const field of ['name', 'email']) {
+        validations.push(new RequiredFieldValidation(field))
+    }
+    return new ValidationComposite(validations)
+}
