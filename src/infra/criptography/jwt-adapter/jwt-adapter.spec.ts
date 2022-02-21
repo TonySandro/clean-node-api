4866ba7958682cf0b8bb769c2cf99755f43b02c9
TonySandro <tonysduarte101@gmail.com>
2022-02-21 16:40:26 -0300

    test: ensure JwtAdapater  throw if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -33,4 +33,14 @@ describe('Jwt adapter', () => {
 
         expect(accessToken).toBe('any_token')
     })
+
+    test('Should throw if sign throws', async () => {
+        const { sut } = makeSut()
+        jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        const promise = sut.encrypt('any_id')
+        expect(promise).rejects.toThrow()
+    })
 })
