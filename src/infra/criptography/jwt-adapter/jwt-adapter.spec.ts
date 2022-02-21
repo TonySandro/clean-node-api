03365f3d7cf8c39722eba46d4a476cff05f26c5f
TonySandro <tonysduarte101@gmail.com>
2022-02-21 16:36:42 -0300

    test: ensure JwtAdapater retuns a token when sign succeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+    async sign(): Promise<string> {
+        return new Promise(resolve => resolve('any_token'))
+    }
+}))
+
 interface SutTypes {
     sut: JwtAdapter
 }
@@ -19,5 +25,12 @@ describe('Jwt adapter', () => {
 
         await sut.encrypt('any_id')
         expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+    })
+
+    test('Should return a token on sign success', async () => {
+        const { sut } = makeSut()
+        const accessToken = await sut.encrypt('any_id')
+
+        expect(accessToken).toBe('any_token')
     })
 })
