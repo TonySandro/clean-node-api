8a49dfcd883ca2ddf8af3fc7abd258f02e5b2203
TonySandro <tonysduarte101@gmail.com>
2022-02-21 22:52:38 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -59,4 +59,21 @@ describe('Account Mongo Repository', () => {
 
         expect(account).toBeFalsy()
     })
+
+    // test('Should update the account accessToken on updateAccessToken success', async () => {
+    //     const sut = makeSut()
+    //     const result = await accountCollection.insertOne({
+    //         name: 'any_name',
+    //         email: 'any_email@email.com',
+    //         password: 'any_password',
+    //     })
+
+    //     // expect(result[0].accessToken).toBeFalsy()
+    //     await sut.updateAccessToken(`${result.insertedId}`, 'any_token')
+    //     const account = await accountCollection.findOne({ _id: result.insertedId })
+    //     console.log(account)
+
+    //     expect(account).toBeTruthy()
+    //     expect(account.accessToken).toBe('any_token')
+    // })
 })
